a03f1d97065ae6993e78df78aeb6eebaaf464b8a
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-03-24 13:17:17 +0900

    Update view_data.py

--- nuscenes/scripts/view_data.py
+++ nuscenes/scripts/view_data.py
@@ -24,7 +24,7 @@ def setup(cfg):
         cfg.shuffle = False
 
 
-@hydra.main(config_path=/content/CoBEVT/nuscenes/config / 'config', config_name='config.yaml')
+@hydra.main(config_path= https://github.com/whysop1/CoBEVT/tree/main/nuscenes/config / 'config', config_name='config.yaml')
 def main(cfg):
     setup_config(cfg, setup)
 
