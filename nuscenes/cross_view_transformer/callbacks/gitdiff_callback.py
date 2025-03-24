21b584f8587551765ea84cbe4b0d4c5ef673a228
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-03-24 19:45:25 +0900

    Update gitdiff_callback.py

--- nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
+++ nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
@@ -1,45 +1,4 @@
-import logging
-import pytorch_lightning as pl
-import git
-from pathlib import Path
-from pytorch_lightning.utilities import rank_zero_only
-from omegaconf import OmegaConf, DictConfig
 
-log = logging.getLogger(__name__)
-
-PROJECT_ROOT = Path(__file__).parent.parent.parent
-TEMPLATE = """
-==================================================
-{diff}
-==================================================
-{cfg}
-==================================================
-"""
-
-class GitDiffCallback(pl.Callback):
-    """
-    Prints git diff and config
-    """
-    def __init__(self, cfg: DictConfig):
-        super().__init__()
-
-        self.cfg = cfg
-
-    @rank_zero_only
-    def on_fit_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
-        # Git diff 가져오기
-        repo = git.Repo(PROJECT_ROOT)
-        diff = repo.git.diff()  # 실제 git diff를 가져옴
-
-        # cfg를 YAML로 변환
-        cfg = OmegaConf.to_yaml(self.cfg)
-
-        # 템플릿에 diff와 cfg를 포함하여 로그 출력
-        log.info(TEMPLATE.format(diff=diff, cfg=cfg))
-
-
-
-'''
 import logging
 import pytorch_lightning as pl
 import git
@@ -77,4 +36,3 @@ class GitDiffCallback(pl.Callback):
         cfg = OmegaConf.to_yaml(self.cfg)
 
         log.info(TEMPLATE.format(cfg=cfg))
-'''
