2632f89f141a872b68a9dc884fc74875f9642fe8
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-03-27 18:31:43 +0900

    Update gitdiff_callback.py

--- nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
+++ nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
@@ -11,7 +11,7 @@ from omegaconf import OmegaConf, DictConfig
 log = logging.getLogger(__name__)
 
 
-PROJECT_ROOT = 'CoBEVT/nuscenes'
+PROJECT_ROOT = Path("/content/CoBEVT")
 TEMPLATE = """
 ==================================================
 {diff}
