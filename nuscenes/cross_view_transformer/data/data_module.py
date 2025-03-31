72a20085d7474ed3c99141b1385d0b73918fccf9
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-03-31 13:01:41 +0900

    Update data_module.py

--- nuscenes/cross_view_transformer/data/data_module.py
+++ nuscenes/cross_view_transformer/data/data_module.py
@@ -1,3 +1,4 @@
+'''
 import torch
 import pytorch_lightning as pl
 
@@ -33,5 +34,39 @@ class DataModule(pl.LightningDataModule):
 
     def val_dataloader(self, shuffle=True):
         return self.get_split('val', loader=True, shuffle=shuffle)
+
+'''
+
+class DataModule(pl.LightningDataModule):
+    def __init__(self, dataset: str, data_config: dict, loader_config: dict):
+        super().__init__()
+
+        self.get_data = get_dataset_module_by_name(dataset).get_data
+        self.data_config = data_config
+        self.loader_config = loader_config
+
+        # 🔧 num_workers를 2로 고정
+        self.loader_config["num_workers"] = 2
+
+    def get_split(self, split, loader=True, shuffle=False):
+        datasets = self.get_data(split=split, **self.data_config)
+
+        if not loader:
+            return datasets
+
+        dataset = torch.utils.data.ConcatDataset(datasets)
+        loader_config = dict(self.loader_config)
+
+        # 🔧 num_workers가 0이면 prefetch_factor 제거
+        if loader_config["num_workers"] == 0:
+            loader_config.pop("prefetch_factor", None)
+
+        return torch.utils.data.DataLoader(dataset, shuffle=shuffle, **loader_config)
+
+    def train_dataloader(self, shuffle=True):
+        return self.get_split("train", loader=True, shuffle=shuffle)
+
+    def val_dataloader(self, shuffle=True):
+        return self.get_split("val", loader=True, shuffle=shuffle)
 
 
