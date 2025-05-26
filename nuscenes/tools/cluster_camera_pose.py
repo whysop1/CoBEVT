f1c544f7d1724282a4aabeb09777038e56e1dafb
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-05-26 20:53:46 +0900

    Update cluster_camera_pose.py

--- nuscenes/tools/cluster_camera_pose.py
+++ nuscenes/tools/cluster_camera_pose.py
@@ -19,8 +19,6 @@ def main(nusc_path, version='v1.0-trainval', num_clusters=10, out_path='cluster_
     
     nusc = NuScenes(version='v1.0-trainval', dataroot=nusc_path)
 
-    if hasattr(nusc, 'maps'):
-    nusc.maps = []
     
     camera_poses = []
     tokens = []
