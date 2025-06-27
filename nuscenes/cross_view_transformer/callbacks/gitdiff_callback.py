cb82de0f48eab88008d74fa4bbd26b29c8220edd
whysop1 <127463323+whysop1@users.noreply.github.com>
2025-06-27 15:57:43 +0900

    Update gitdiff_callback.py

--- nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
+++ nuscenes/cross_view_transformer/callbacks/gitdiff_callback.py
@@ -1,42 +1,4 @@
-'''
-import logging
-import pytorch_lightning as pl
-import git
 
-from pathlib import Path
-from pytorch_lightning.utilities import rank_zero_only
-from omegaconf import OmegaConf, DictConfig
-
-
-log = logging.getLogger(__name__)
-
-
-PROJECT_ROOT = Path("CoBEVT/nuscenes")
-TEMPLATE = """
-==================================================
-{diff}
-==================================================
-{cfg}
-==================================================
-"""
-
-
-class GitDiffCallback(pl.Callback):
-    """
-    Prints git diff and config
-    """
-    def __init__(self, cfg: DictConfig):
-        super().__init__()
-
-        self.cfg = cfg
-
-    @rank_zero_only
-    def on_fit_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
-        diff = git.Repo(PROJECT_ROOT).git.diff()
-        cfg = OmegaConf.to_yaml(self.cfg)
-
-        log.info(TEMPLATE.format(cfg=cfg))
-'''
 
 import logging
 import pytorch_lightning as pl
