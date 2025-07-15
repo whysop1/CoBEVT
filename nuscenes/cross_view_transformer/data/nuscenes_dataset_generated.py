44061062a1043ae3787088fbb766c96dc4832ce7
defenxe <yong020504@gmail.com>
2025-07-15 18:33:15 +0900

    Update nuscenes_dataset_generated.py

--- nuscenes/cross_view_transformer/data/nuscenes_dataset_generated.py
+++ nuscenes/cross_view_transformer/data/nuscenes_dataset_generated.py
@@ -55,224 +55,89 @@ class NuScenesGeneratedDataset(torch.utils.data.Dataset):
         return data
 '''
 
+import json
 import torch
 import numpy as np
 import cv2
 
 from pathlib import Path
-from functools import lru_cache
-
 from pyquaternion import Quaternion
-from shapely.geometry import MultiPolygon
+from nuscenes.utils import data_classes
 
-from .common import INTERPOLATION, get_view_matrix, get_pose, get_split
-from .transforms import Sample, SaveDataTransform
+from .common import get_split
+from .transforms import Sample, LoadDataTransform
 
-
-STATIC = ['lane', 'road_segment']
-DIVIDER = ['road_divider', 'lane_divider']
-DYNAMIC = [
-    'car', 'truck', 'bus',
-    'trailer', 'construction',
-    'pedestrian',
-    'motorcycle', 'bicycle',
-]
-
-CLASSES = STATIC + DIVIDER + DYNAMIC
-NUM_CLASSES = len(CLASSES)
-
+INTERPOLATION = cv2.INTER_NEAREST
 
 def get_data(
     dataset_dir,
     labels_dir,
     split,
     version,
-    dataset='unused',                   # ignore
-    augment='unused',                   # ignore
-    image=None,                     # ignore
-    label_indices='unused',             # ignore
-    num_classes=NUM_CLASSES,            # in here to make config consistent
+    num_classes,
+    augment='none',
+    image=None,
+    dataset='unused',
     **dataset_kwargs
 ):
-    assert num_classes == NUM_CLASSES
+    dataset_dir = Path(dataset_dir)
+    labels_dir = Path(labels_dir)
 
-    helper = NuScenesSingleton(dataset_dir, version)
-    transform = SaveDataTransform(labels_dir)
+    augment = 'none' if split != 'train' else augment
+    transform = LoadDataTransform(dataset_dir, labels_dir, image, num_classes, augment)
 
-    # Format the split name
     split = f'mini_{split}' if version == 'v1.0-mini' else split
     split_scenes = get_split(split, 'nuscenes')
 
-    result = list()
-
-    for scene_name, scene_record in helper.get_scenes():
-        if scene_name not in split_scenes:
-            continue
-
-        data = NuScenesDataset(scene_name, scene_record, helper,
-                               transform=transform, **dataset_kwargs)
-        result.append(data)
-
-    return result
+    return [NuScenesGeneratedDataset(s, labels_dir, transform=transform) for s in split_scenes]
 
 
-class NuScenesSingleton:
+class NuScenesGeneratedDataset(torch.utils.data.Dataset):
     """
-    Wraps both nuScenes and nuScenes map API
-
-    This was an attempt to sidestep the 30 second loading time in a "clean" manner
+    Loads JSON and recomputes object_count on-the-fly.
     """
-    def __init__(self, dataset_dir, version):
-        """
-        dataset_dir: /path/to/nuscenes/
-        version: v1.0-trainval
-        """
-        self.dataroot = str(dataset_dir)
-        self.nusc = self.lazy_nusc(version, self.dataroot)
 
-    @classmethod
-    def lazy_nusc(cls, version, dataroot):
-        # Import here so we don't require nuscenes-devkit unless regenerating labels
-        from nuscenes.nuscenes import NuScenes
-
-        if not hasattr(cls, '_lazy_nusc'):
-            cls._lazy_nusc = NuScenes(version=version, dataroot=dataroot)
-
-        return cls._lazy_nusc
-
-    def get_scenes(self):
-        for scene_record in self.nusc.scene:
-            yield scene_record['name'], scene_record
-
-    @lru_cache(maxsize=16)
-    def get_map(self, log_token):
-        # Import here so we don't require nuscenes-devkit unless regenerating labels
-        from nuscenes.map_expansion.map_api import NuScenesMap
-
-        map_name = self.nusc.get('log', log_token)['location']
-        nusc_map = NuScenesMap(dataroot=self.dataroot, map_name=map_name)
-
-        return nusc_map
-
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_singleton'):
-            obj = super(NuScenesSingleton, cls).__new__(cls)
-            obj.__init__(*args, **kwargs)
-
-            cls._singleton = obj
-
-        return cls._singleton
-
-
-class NuScenesDataset(torch.utils.data.Dataset):
-    CAMERAS = ['CAM_FRONT_LEFT', 'CAM_FRONT', 'CAM_FRONT_RIGHT',
-               'CAM_BACK_LEFT', 'CAM_BACK', 'CAM_BACK_RIGHT']
-
-    def __init__(
-        self,
-        scene_name: str,
-        scene_record: dict,
-        helper: NuScenesSingleton,
-        transform=None,
-        cameras=[[0, 1, 2, 3, 4, 5]],
-        bev={'h': 200, 'w': 200, 'h_meters': 100, 'w_meters': 100, 'offset': 0.0},
-    ):
-        self.scene_name = scene_name
+    def __init__(self, scene_name, labels_dir, transform=None):
+        self.samples = json.loads((Path(labels_dir) / f'{scene_name}.json').read_text())
         self.transform = transform
+        self.bev_shape = (200, 200)
 
-        self.nusc = helper.nusc
-        self.nusc_map = helper.get_map(scene_record['log_token'])
+    def __len__(self):
+        return len(self.samples)
 
-        self.view = get_view_matrix(**bev)
-        self.bev_shape = (bev['h'], bev['w'])
+    def __getitem__(self, idx):
+        # load sample as dict
+        data_dict = dict(self.samples[idx])
 
-        self.samples = self.parse_scene(scene_record, cameras)
+        # get object_count
+        annotations = data_dict.get('annotations', [])
+        view = np.array(data_dict['view'])
+        pose = np.array(data_dict['pose'])
+        pose_inverse = np.array(data_dict['pose_inverse'])
 
-    def parse_scene(self, scene_record, camera_rigs):
-        data = []
-        sample_token = scene_record['first_sample_token']
+        object_count = self.compute_object_count(annotations, view, pose, pose_inverse)
 
-        while sample_token:
-            sample_record = self.nusc.get('sample', sample_token)
+        # add it back
+        data_dict['object_count'] = object_count
 
-            for camera_rig in camera_rigs:
-                data.append(self.parse_sample_record(sample_record, camera_rig))
+        # wrap in Sample
+        data = Sample(**data_dict)
 
-            sample_token = sample_record['next']
+        if self.transform is not None:
+            data = self.transform(data)
 
         return data
 
-    def parse_pose(self, record, *args, **kwargs):
-        return get_pose(record['rotation'], record['translation'], *args, **kwargs)
+    def compute_object_count(self, annotations, view, pose, pose_inverse):
+        h, w = self.bev_shape
 
-    def parse_sample_record(self, sample_record, camera_rig):
-        lidar_record = self.nusc.get('sample_data', sample_record['data']['LIDAR_TOP'])
-        egolidar = self.nusc.get('ego_pose', lidar_record['ego_pose_token'])
-
-        world_from_egolidarflat = self.parse_pose(egolidar, flat=True)
-        egolidarflat_from_world = self.parse_pose(egolidar, flat=True, inv=True)
-
-        cam_channels = []
-        images = []
-        intrinsics = []
-        extrinsics = []
-
-        for cam_idx in camera_rig:
-            cam_channel = self.CAMERAS[cam_idx]
-            cam_token = sample_record['data'][cam_channel]
-
-            cam_record = self.nusc.get('sample_data', cam_token)
-            egocam = self.nusc.get('ego_pose', cam_record['ego_pose_token'])
-            cam = self.nusc.get('calibrated_sensor', cam_record['calibrated_sensor_token'])
-
-            cam_from_egocam = self.parse_pose(cam, inv=True)
-            egocam_from_world = self.parse_pose(egocam, inv=True)
-
-            E = cam_from_egocam @ egocam_from_world @ world_from_egolidarflat
-            I = cam['camera_intrinsic']
-
-            full_path = Path(self.nusc.get_sample_data_path(cam_token))
-            image_path = str(full_path.relative_to(self.nusc.dataroot))
-
-            cam_channels.append(cam_channel)
-            intrinsics.append(I)
-            extrinsics.append(E.tolist())
-            images.append(image_path)
-
-        return {
-            'scene': self.scene_name,
-            'token': sample_record['token'],
-
-            'pose': world_from_egolidarflat.tolist(),
-            'pose_inverse': egolidarflat_from_world.tolist(),
-
-            'cam_ids': list(camera_rig),
-            'cam_channels': cam_channels,
-            'intrinsics': intrinsics,
-            'extrinsics': extrinsics,
-            'images': images,
-        }
-
-    def get_dynamic_objects(self, sample, annotations): #카메라 이미지 내 객체 수를 세는 object_count 역할 수행 함수임
-        h, w = self.bev_shape[:2]
-
-        segmentation = np.zeros((h, w), dtype=np.uint8)
-        center_score = np.zeros((h, w), dtype=np.float32)
-        center_offset = np.zeros((h, w, 2), dtype=np.float32)
-        center_ohw = np.zeros((h, w, 4), dtype=np.float32)
+        # Preallocate
         buf = np.zeros((h, w), dtype=np.uint8)
-
-        visibility = np.full((h, w), 255, dtype=np.uint8)
-
-        coords = np.stack(np.meshgrid(np.arange(w), np.arange(h)), -1).astype(np.float32)
-
         object_count = 0
-        
-        for ann, p in zip(annotations, self.convert_to_box(sample, annotations)):
+
+        # convert annotations to BEV polygons
+        for ann, p in zip(annotations, self.convert_to_box(annotations, view, pose_inverse)):
             box = p[:2, :4]
-            center = p[:2, 4]
-            front = p[:2, 5]
-            left = p[:2, 6]
 
             buf.fill(0)
             cv2.fillPoly(buf, [box.round().astype(np.int32).T], 1, INTERPOLATION)
@@ -281,213 +146,30 @@ class NuScenesDataset(torch.utils.data.Dataset):
             if not np.count_nonzero(mask):
                 continue
 
-            sigma = 1
-            segmentation[mask] = 255
-            center_offset[mask] = center[None] - coords[mask]
-            center_score[mask] = np.exp(-(center_offset[mask] ** 2).sum(-1) / (sigma ** 2))
-
-            # orientation, h/2, w/2
-            center_ohw[mask, 0:2] = ((front - center) / (np.linalg.norm(front - center) + 1e-6))[None]
-            center_ohw[mask, 2:3] = np.linalg.norm(front - center)
-            center_ohw[mask, 3:4] = np.linalg.norm(left - center)
-
-            visibility[mask] = ann['visibility_token']
-
             object_count += 1
 
-
-
-        segmentation = np.float32(segmentation[..., None])
-        center_score = center_score[..., None]
-
-        result = np.concatenate((segmentation, center_score, center_offset, center_ohw), 2)
-
         print(object_count)
+        return object_count
 
-        # (h, w, 1 + 1 + 2 + 2)
-        return result, visibility, object_count #return 값에 object_count 추가
-
-    def convert_to_box(self, sample, annotations):
-        # Import here so we don't require nuscenes-devkit unless regenerating labels
-        from nuscenes.utils import data_classes
-
-        V = self.view
-        M_inv = np.array(sample['pose_inverse'])
+    def convert_to_box(self, annotations, view, pose_inverse):
+        M_inv = np.array(pose_inverse)
         S = np.array([
             [1, 0, 0, 0],
             [0, 1, 0, 0],
             [0, 0, 0, 1],
         ])
 
-        for a in annotations:
-            box = data_classes.Box(a['translation'], a['size'], Quaternion(a['rotation']))
+        for ann in annotations:
+            box = data_classes.Box(ann['translation'], ann['size'], Quaternion(ann['rotation']))
 
-            corners = box.bottom_corners()                                              # 3 4
-            center = corners.mean(-1)                                                   # 3
-            front = (corners[:, 0] + corners[:, 1]) / 2.0                               # 3
-            left = (corners[:, 0] + corners[:, 3]) / 2.0                                # 3
+            corners = box.bottom_corners()
+            center = corners.mean(-1)
+            front = (corners[:, 0] + corners[:, 1]) / 2.0
+            left = (corners[:, 0] + corners[:, 3]) / 2.0
 
-            p = np.concatenate((corners, np.stack((center, front, left), -1)), -1)      # 3 7
-            p = np.pad(p, ((0, 1), (0, 0)), constant_values=1.0)                        # 4 7
-            p = V @ S @ M_inv @ p                                                       # 3 7
+            p = np.concatenate((corners, np.stack((center, front, left), -1)), -1)
+            p = np.pad(p, ((0, 1), (0, 0)), constant_values=1.0)
+            p = view @ S @ M_inv @ p
 
-            yield p                                                                     # 3 7
+            yield p
 
-    def get_category_index(self, name, categories):
-        """
-        human.pedestrian.adult
-        """
-        tokens = name.split('.')
-
-        for i, category in enumerate(categories):
-            if category in tokens:
-                return i
-
-        return None
-
-    def get_annotations_by_category(self, sample, categories):
-        result = [[] for _ in categories]
-
-        for ann_token in self.nusc.get('sample', sample['token'])['anns']:
-            a = self.nusc.get('sample_annotation', ann_token)
-            idx = self.get_category_index(a['category_name'], categories)
-
-            if idx is not None:
-                result[idx].append(a)
-
-        return result
-
-    def get_line_layers(self, sample, layers, patch_radius=150, thickness=1):
-        h, w = self.bev_shape[:2]
-        V = self.view
-        M_inv = np.array(sample['pose_inverse'])
-        S = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 0, 1],
-        ])
-
-        box_coords = (sample['pose'][0][-1] - patch_radius, sample['pose'][1][-1] - patch_radius,
-                      sample['pose'][0][-1] + patch_radius, sample['pose'][1][-1] + patch_radius)
-        records_in_patch = self.nusc_map.get_records_in_patch(box_coords, layers, 'intersect')
-
-        result = list()
-
-        for layer in layers:
-            render = np.zeros((h, w), dtype=np.uint8)
-
-            for r in records_in_patch[layer]:
-                polygon_token = self.nusc_map.get(layer, r)
-                line = self.nusc_map.extract_line(polygon_token['line_token'])
-
-                p = np.float32(line.xy)                                     # 2 n
-                p = np.pad(p, ((0, 1), (0, 0)), constant_values=0.0)        # 3 n
-                p = np.pad(p, ((0, 1), (0, 0)), constant_values=1.0)        # 4 n
-                p = V @ S @ M_inv @ p                                       # 3 n
-                p = p[:2].round().astype(np.int32).T                        # n 2
-
-                cv2.polylines(render, [p], False, 1, thickness=thickness)
-
-            result.append(render)
-
-        return 255 * np.stack(result, -1)
-
-    def get_static_layers(self, sample, layers, patch_radius=150):
-        h, w = self.bev_shape[:2]
-        V = self.view
-        M_inv = np.array(sample['pose_inverse'])
-        S = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 0, 1],
-        ])
-
-        box_coords = (sample['pose'][0][-1] - patch_radius, sample['pose'][1][-1] - patch_radius,
-                      sample['pose'][0][-1] + patch_radius, sample['pose'][1][-1] + patch_radius)
-        records_in_patch = self.nusc_map.get_records_in_patch(box_coords, layers, 'intersect')
-
-        result = list()
-
-        for layer in layers:
-            render = np.zeros((h, w), dtype=np.uint8)
-
-            for r in records_in_patch[layer]:
-                polygon_token = self.nusc_map.get(layer, r)
-
-                if layer == 'drivable_area': polygon_tokens = polygon_token['polygon_tokens']
-                else: polygon_tokens = [polygon_token['polygon_token']]
-
-                for p in polygon_tokens:
-                    polygon = self.nusc_map.extract_polygon(p)
-                    polygon = MultiPolygon([polygon])
-
-                    exteriors = [np.array(poly.exterior.coords).T for poly in polygon.geoms]
-                    exteriors = [np.pad(p, ((0, 1), (0, 0)), constant_values=0.0) for p in exteriors]
-                    exteriors = [np.pad(p, ((0, 1), (0, 0)), constant_values=1.0) for p in exteriors]
-                    exteriors = [V @ S @ M_inv @ p for p in exteriors]
-                    exteriors = [p[:2].round().astype(np.int32).T for p in exteriors]
-
-                    cv2.fillPoly(render, exteriors, 1, INTERPOLATION)
-
-                    interiors = [np.array(pi.coords).T for poly in polygon.geoms for pi in poly.interiors]
-                    interiors = [np.pad(p, ((0, 1), (0, 0)), constant_values=0.0) for p in interiors]
-                    interiors = [np.pad(p, ((0, 1), (0, 0)), constant_values=1.0) for p in interiors]
-                    interiors = [V @ S @ M_inv @ p for p in interiors]
-                    interiors = [p[:2].round().astype(np.int32).T for p in interiors]
-
-                    cv2.fillPoly(render, interiors, 0, INTERPOLATION)
-
-            result.append(render)
-
-        return 255 * np.stack(result, -1)
-
-    def get_dynamic_layers(self, sample, anns_by_category):
-        h, w = self.bev_shape[:2]
-        result = list()
-
-        for anns in anns_by_category:
-            render = np.zeros((h, w), dtype=np.uint8)
-
-            for p in self.convert_to_box(sample, anns):
-                p = p[:2, :4]
-
-                cv2.fillPoly(render, [p.round().astype(np.int32).T], 1, INTERPOLATION)
-
-            result.append(render)
-
-        return 255 * np.stack(result, -1)
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, idx):
-        sample = self.samples[idx]
-
-        # Raw annotations
-        anns_dynamic = self.get_annotations_by_category(sample, DYNAMIC)
-        anns_vehicle = self.get_annotations_by_category(sample, ['vehicle'])[0]
-
-        static = self.get_static_layers(sample, STATIC)                             # 200 200 2
-        dividers = self.get_line_layers(sample, DIVIDER)                            # 200 200 2
-        dynamic = self.get_dynamic_layers(sample, anns_dynamic)                     # 200 200 8
-        bev = np.concatenate((static, dividers, dynamic), -1)                       # 200 200 12
-
-        assert bev.shape[2] == NUM_CLASSES
-
-        # Additional labels for vehicles only.
-        aux, visibility, object_count = self.get_dynamic_objects(sample, anns_vehicle)
-
-        # Package the data.
-        data = Sample(
-            view=self.view.tolist(),
-            bev=bev,
-            aux=aux,
-            visibility=visibility,
-            object_count = object_count,
-            **sample
-        )
-
-        if self.transform is not None:
-            data = self.transform(data)
-
-        return data
